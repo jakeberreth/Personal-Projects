3e5c378aba48271ce63e8084b5fc7dfe778a77df
jakeberreth <47266242+jakeberreth@users.noreply.github.com>
2020-06-17 10:29:03 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/***************************
+Name: Jake Berreth
+Project: Battleship
+Date: February 15, 2020
+
+****************************/
+
+#include "Battleship.h"
+
+//Execution starts here
+int main()
+{
+	run_game();
+
+	return 0;
+}
